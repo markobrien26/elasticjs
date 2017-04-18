e5a4390a572e97905e381955620c792ea8287ee5
markobrien26 <20061938@mail.wit.ie>
2017-04-18 22:04:38 +0000

    updated

--- sshpass.js
+++ sshpass.js
@@ -1,11 +1,11 @@
-
+var request = require("request");
 
 var client = require('./connection.js');
 
 client.search({  
   index: 'logstash-2017.03.07',
   type: 'syslog',
-  fields: ["src_ip","geoip.country_name"],
+  fields: ["src_ip"],
   size: 20,
   body: {
     query: {
@@ -32,7 +32,18 @@ client.search({
       console.log('Total hits: ',response.hits.total);
       console.log("--- Hits ---");
       response.hits.hits.forEach(function(hit){
-        console.log(hit.fields);
+	var options = { method: 'POST',
+	  url: 'http://localhost:3000/tasks',
+	  headers:
+	     {'content-type': 'application/x-www-form-urlencoded' },
+	  form: { src_ip: hit.fields.src_ip } };
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+	  console.log(body);
+	});
+
+        console.log(hit.fields.src_ip);
       })
     }
 });
